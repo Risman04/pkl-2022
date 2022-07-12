361976810a1ed40238b510f4a3b6046e6de2bb87
Risman04 <risman4205@gmail.com>
2022-07-12 15:15:32 +0700

    belajar laravel

---
+++ resources/views/pages/pemesanan.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Pemesanan</legend>
+
+        @if($menu1 == null)
+            <strong>{{$menu1}}</strong>
+        @elseif($menu1 == 'mie goreng')         
+            Makanan : <strong>{{$menu1}}</strong><br>
+            Harga : Rp.5.000
+            <hr>
+        @elseif($menu1 == 'seblak')
+            Makanan : <strong>{{$menu1}}</strong><br>
+            Harga : Rp.7.500
+            <hr>
+        @elseif($menu1 == 'nasi padang')
+            Makanan : <strong>{{$menu1}}</strong><br>
+            Harga : Rp.15.000
+            <hr>
+        @else
+            Pesanan Anda : <strong>{{$menu1}}</strong><br>
+            <hr>
+        @endif
+
+        @if($menu2 == null)
+            <strong>{{$menu2}}</strong>
+        @elseif($menu2 == 'teh')         
+            Minuman : <strong>{{$menu2}}</strong><br>
+            Harga : Rp.5.000
+            <hr>
+        @elseif($menu2 == 'kopi')
+            Minuman : <strong>{{$menu2}}</strong><br>
+            Harga : Rp.7.500
+            <hr>
+        @elseif($menu2 == 'jus')
+            Minuman : <strong>{{$menu2}}</strong><br>
+            Harga : Rp.10.000
+            <hr>
+        @else
+            Pesanan Anda : <strong>{{$menu2}}</strong><br>
+            <hr>
+        @endif
+
+        @if($menu3 == null)
+            <strong>{{$menu3}}</strong>
+        @elseif($menu3 == 'kecil')         
+            Porsi : <strong>{{$menu3}}</strong><br>
+            Harga : Rp.2.500
+            <hr>
+        @elseif($menu3 == 'sedang')
+            Porsi : <strong>{{$menu3}}</strong><br>
+            Harga : Rp.5.000
+            <hr>
+        @elseif($menu3 == 'besar')
+            Porsi : <strong>{{$menu3}}</strong><br>
+            Harga : Rp.10.000
+            <hr>
+        @else
+            Pesanan Anda : <strong>{{$menu3}}</strong><br>
+            <hr>
+        @endif
+
+    </fieldset>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,47 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//basic route
+Route::get('halaman-2', function () {
+    return "halaman 2";
+});
+//basic route
+Route::get('halaman-3', function () {
+    return "halaman 3";
+});
+//contoh parameter
+Route::get('halaman-3/{nama}', function ($nama) {
+    return "halaman 3 " .$nama;
+});
+//contoh parameter
+Route::get('halaman-profile/{nama}/jenis-kelamin/{jk}', function ($nama, $jk) {
+    return "halaman profile : " .$nama ." Dengan jenis kelamin : " .$jk;
+});
+
+//opsional parameter
+Route::get('halaman-profile/{nama?}', function ($nama = "Belum ada nama") {
+    return "halaman profile : " .$nama;
+});
+
+// latihan
+Route::get('about/{nama}', function ($nama) {
+    // return "halaman profile : " .$nama;
+    return view('about', compact('nama'));
+});
+
+Route::get('about/{nama}', function ($nama) {
+    // return "halaman profile : " .$nama;
+    $calculator = 1 + 5;
+    return view('about', compact('nama', 'calculator'));
+});
+
+
+//latihan kang candra
+Route::get('pesan/{menu?}', function ($menu = "silahkan masukan pesanan") {
+    return view('pages.pesan', compact('menu'));
+});
+
+Route::get('pemesanan/{menu1?}/{menu2?}/{menu3?}', function ($menu1 = "silahkan masukan pesanan", $menu2 = null, $menu3 = null) {
+    return view('pages.pemesanan', compact('menu1', 'menu2','menu3'));
+});
